08580bf37e0d44a4e41dd0d56135e0f8e975c97a
jessie <164967373@qq.com>
2018-01-05 17:40:24 +0800

    f

--- src/main/java/com/cyy/Recv.java
+++ src/main/java/com/cyy/Recv.java
@@ -16,14 +16,31 @@ public class Recv {
         channel.queueDeclare(QUEUE_NAME, false, false, false, null);
         System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
 
-        Consumer consumer = new DefaultConsumer(channel) {
+        final Consumer consumer = new DefaultConsumer(channel) {
             @Override
-            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
-                    throws IOException {
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
                 String message = new String(body, "UTF-8");
+
                 System.out.println(" [x] Received '" + message + "'");
+                try {
+                    doWork(message);
+                } catch (InterruptedException e) {
+                    System.out.println(e.getMessage());
+                } finally {
+                    System.out.println(" [x] Done");
+                }
             }
         };
-        channel.basicConsume(QUEUE_NAME, true, consumer);
+        boolean autoAck = true; // acknowledgment is covered below
+        channel.basicConsume(QUEUE_NAME, autoAck, consumer);
+    }
+
+
+
+
+    private static void doWork(String task) throws InterruptedException {
+        for (char ch: task.toCharArray()) {
+            if (ch == '.') Thread.sleep(1000);
+        }
     }
 }
--- src/main/java/com/cyy/Send.java
+++ src/main/java/com/cyy/Send.java
@@ -14,11 +14,28 @@ public class Send {
         Channel channel = connection.createChannel();
 
         channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-        String message = "Hello World!";
-        channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
+
+        String message = getMessage(argv);
+        channel.basicPublish("", "hello", null, message.getBytes());
         System.out.println(" [x] Sent '" + message + "'");
 
         channel.close();
         connection.close();
+    }
+
+    private static String getMessage(String[] strings){
+        if (strings.length < 1)
+            return "Hello World!";
+        return joinStrings(strings, " ");
+    }
+
+    private static String joinStrings(String[] strings, String delimiter) {
+        int length = strings.length;
+        if (length == 0) return "";
+        StringBuilder words = new StringBuilder(strings[0]);
+        for (int i = 1; i < length; i++) {
+            words.append(delimiter).append(strings[i]);
+        }
+        return words.toString();
     }
 }
